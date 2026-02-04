2a14abe97c6f3aeef0f9223a1554b0eec5767834
Mennovos <mennodvos@gmail.com>
2026-02-04 10:01:27 +0100

    Feat: Move

--- MineRunner/Assets/Scripts/PlayerScript.cs
+++ MineRunner/Assets/Scripts/PlayerScript.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private float Speed;
+    [SerializeField] private Transform Cam;
+    private Vector3 Movement;
 
+    private Controls Controls;
+
+    private Rigidbody Rb;
+   // private Animator Anim;
     private void Awake()
     {
-        
+       // Anim = GetComponent<Animator>();
+        Controls = new Controls();
+        Controls.Player.Enable();
+
+        Controls.Player.Move.performed += OnMove;
+        Controls.Player.Move.canceled += OnMove;
+
+        Rb = GetComponent<Rigidbody>();
     }
-  
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        Movement.x = context.ReadValue<Vector2>().x;
+        Movement.z = context.ReadValue<Vector2>().y;
+        bool Walking = input.sqrMagnitude > 0.01f;
+        //Anim.SetBool("CanWalk", Walking);
+    }
+    private void Update()
+    {
+        transform.Translate(Movement * Speed * Time.deltaTime);
+    }
+
 }
