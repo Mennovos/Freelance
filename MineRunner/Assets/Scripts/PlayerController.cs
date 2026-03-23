5e833edd5c87d4af4dd0d4d553cd60b20d6eab71
Mennovos <mennodvos@gmail.com>
2026-03-23 14:37:43 +0100

    Feat: Camera works

--- MineRunner/Assets/Scripts/PlayerController.cs
+++ MineRunner/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private bool shouldFaceMoveDirection = false;
+
+    private Vector2 moveInput;
+
+
     [SerializeField] private float Speed;
     [SerializeField] private Transform Cam;
     private Vector3 Movement;
@@ -12,9 +21,10 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody Rb;
     private Animator Anim;
     private bool attacking;
+
     private void Awake()
     {
-         Anim = GetComponent<Animator>();
+        Anim = GetComponent<Animator>();
         Controls = new Controls();
         Controls.Player.Enable();
 
@@ -26,7 +36,6 @@ public class PlayerScript : MonoBehaviour
 
         Rb = GetComponent<Rigidbody>();
     }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
@@ -39,7 +48,7 @@ public class PlayerScript : MonoBehaviour
     {
         if (context.performed)
         {
-           Anim.SetTrigger("Punching");
+            Anim.SetTrigger("Punching");
         }
     }
     public void Kick(InputAction.CallbackContext context)
@@ -49,20 +58,36 @@ public class PlayerScript : MonoBehaviour
             Anim.SetTrigger("Kicking");
         }
     }
-    public void DealDamage(float damage)
-    {
-       attacking = true;
-    }
-    private void Update()
+
+    void Update()
     {
         transform.Translate(Movement * Speed * Time.deltaTime);
+
+        //Vector3 forward = cameraTransform.forward;
+        //Vector3 right = cameraTransform.right;
+
+        //forward.y = 0;
+        //right.y = 0;
+
+        //forward.Normalize();
+        //right.Normalize();
+
+        //Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
+     
+
+        //if (shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        //{
+        //    Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        //}
+
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<Health>().TakeDamage(10);
         }
     }
-
+    // Credits Jasperr
 }
